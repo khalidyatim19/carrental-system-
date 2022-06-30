0ca4a63da734fe03a3069602bb8065a78232ada0
khalidyatim19 <2020489056@student.uitm.edu.my>
2022-06-30 08:41:50 +0800

    c3

--- carrental/includes/header.php
+++ carrental/includes/header.php
@@ -9,13 +9,6 @@
         <div class="col-sm-9 col-md-10">
           <div class="header_info">
             <div class="header_widgets">
-              <div class="circle_icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </div>
-              <p class="uppercase_text">For Support Mail us : </p>
-              <a href="mailto:info@example.com">codeprojectsorg@gmail.com</a> </div>
-            <div class="header_widgets">
-              <div class="circle_icon"> <i class="fa fa-phone" aria-hidden="true"></i> </div>
-              <p class="uppercase_text">Service Helpline Call Us: </p>
-              <a href="tel:61-1234-5678-09">+91-9876543210</a> </div>
             <div class="social-follow">
               <ul>
                 <li><a href="https://code-projects.org/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
--- carrental/index.php
+++ carrental/index.php
@@ -46,23 +46,6 @@ error_reporting(0);
 <?php include('includes/header.php');?>
 <!-- /Header --> 
 
-<!-- Banners -->
-<section id="banner" class="banner-section">
-  <div class="container">
-    <div class="div_zindex">
-      <div class="row">
-        <div class="col-md-5 col-md-push-7">
-          <div class="banner_content">
-            <h1>Find the right car for you.</h1>
-            <p>We have more than a thousand cars for you to choose. </p>
-            <a href="#" class="btn">Read More <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a> </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- /Banners --> 
-
 
 <!-- Resent Cat-->
 <section class="section-padding gray-bg">
