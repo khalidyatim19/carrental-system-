08d7b8b905046b5764422c8b92189e17ddd77b6b
IzzatMaswan <103076901+IzzatMaswan@users.noreply.github.com>
2022-07-24 20:38:25 +0800

    banner

--- carrental/index.php
+++ carrental/index.php
@@ -53,9 +53,7 @@ error_reporting(0);
       <div class="row">
         <div class="col-md-5 col-md-push-7">
           <div class="banner_content">
-            <h1>Find the right car for you.</h1>
-            <p>We have more than a thousand cars for you to choose. </p>
-            <a href="#" class="btn">Read More <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a> </div>
+
         </div>
       </div>
     </div>
